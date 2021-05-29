ed4305590165d5d7f3ff80dd5de13b4150368fb1
sinzii <thangvx@newayict.com>
2021-05-29 23:30:56 +0700

    Init the website

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    baseUrl: 'https://meblog.sinzii.me',
+    baseContext: '',
+    siteName: 'meblog',
+    siteDescription: 'A simple static site generator',
+    dateTimeFormat: 'DD/MM/YYYY - HH:mm',
+    dateFormat: 'DD/MM/YYYY',
+    postUrlStyle: 'SLUG',
+    me: {
+        fullname: 'Your Name',
+        nickname: '@meblog',
+        github: 'https://github.com/sinzii/meblog'
+    },
+    latestPosts: 5
+}
